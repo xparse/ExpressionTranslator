09926226654fff151f6ccea9f12c9343163c789d
Ivan Shcherbak <alotofall@gmail.com>
2019-05-11 10:46:20 +0300

    upgrade phpunit

--- tests/XpathExpressionTest.php
+++ tests/XpathExpressionTest.php
@@ -3,11 +3,13 @@
 namespace Xparse\ExpressionTranslator\Test;
 
 
+use PHPUnit\Framework\TestCase;
 use Xparse\ExpressionTranslator\XpathExpression;
+
 /**
  * @author Ivan Shcherbak <alotofall@gmail.com>
  */
-class XpathExpressionTest extends \PHPUnit_Framework_TestCase
+class XpathExpressionTest extends TestCase
 {
 
     final public function testAssertSameIO(): void
